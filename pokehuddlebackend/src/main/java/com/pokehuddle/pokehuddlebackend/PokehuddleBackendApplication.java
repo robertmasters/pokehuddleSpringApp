8d983cf8b9e4b1ab589527e915066ae0239212ed
Robert-Rodriguez <robertmasters@hotmail.com>
2022-01-23 22:22:12 -0500

    initialised project using spring initializer site

---
+++ pokehuddlebackend/src/main/java/com/pokehuddle/pokehuddlebackend/PokehuddleBackendApplication.java
@@ -0,0 +1,13 @@
+package com.pokehuddle.pokehuddlebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokehuddleBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokehuddleBackendApplication.class, args);
+	}
+
+}
---
+++ pokehuddlebackend/src/test/java/com/pokehuddle/pokehuddlebackend/PokehuddleBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pokehuddle.pokehuddlebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokehuddleBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
