994b7c4d9a314949a544dd2cef187e88b738bb47
Robert-Rodriguez <robertmasters@hotmail.com>
2022-01-25 17:51:00 -0500

    removed, photos, and articles from seed data, for now I will work with a small amount of entities and expand once app is working

--- pokehuddlebackend/src/main/resources/data.sql
+++ pokehuddlebackend/src/main/resources/data.sql
@@ -4,8 +4,8 @@ FROM userroles;
 DELETE
 FROM roles;
 
-DELETE
-FROM articles;
+--DELETE
+--FROM articles;
 
 DELETE
 FROM preference;
@@ -13,25 +13,20 @@ FROM preference;
 DELETE
 FROM users;
 
-DELETE
-FROM photos;
 
-INSERT INTO users (userid, name, email, password, preferenceid, photoid, articleid)
-    VALUES (1, 'neo', 'neo@pikipika.com', 'pika123', 1,1,1),
-           (2, 'Scarlett', 'scar@pikapika.com', 'pika123', 2,2,2);
+
+INSERT INTO users (userid, name, email, password, preferenceid, articleid)
+    VALUES (1, 'neo', 'neo@pikipika.com', 'pika123',1,1),
+           (2, 'Scarlett', 'scar@pikapika.com', 'pika123', 2,2);
 
 INSERT INTO roles (roleid, name)
     VALUES (1, 'admin'),
            (2, 'member');
 
-INSERT INTO photos (photoid, name, url)
-    VALUES (1, 'pikachu', "no url"),
-           (2, 'ashkool', "no url");
-
-INSERT INTO articles (articleid, title, body, author)
-    VALUES (1, 'Pikachu is too popular', 'Sic de isto et tutius perducit ad actum ipsum, ut si dico Ego autem vadam lavari, ut mens mea in statu naturae conformior. Et similiter circa alias', 'Neo' ),
-           (2, 'Top Ash Mistakes', 'Sic de isto et tutius perducit ad actum ipsum, ut si dico Ego autem vadam', 'Neo'),
-           (3, 'Mew vs Mewtwo', 'Sic de isto et tutius perducit ad actum ipsum, ut si dico “Ego autem vadam', 'Scarlett');
+--INSERT INTO articles (articleid, title, body, author)
+--    VALUES (1, 'Pikachu is too popular', 'Sic de isto et tutius perducit ad actum ipsum, ut si dico Ego autem vadam lavari, ut mens mea in statu naturae conformior. Et similiter circa alias', 'Neo' ),
+--           (2, 'Top Ash Mistakes', 'Sic de isto et tutius perducit ad actum ipsum, ut si dico Ego autem vadam', 'Neo'),
+--           (3, 'Mew vs Mewtwo', 'Sic de isto et tutius perducit ad actum ipsum, ut si dico “Ego autem vadam', 'Scarlett');
 
 INSERT INTO userroles
     VALUES (1, 1),
